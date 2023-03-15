f14a9085388d3b0db074d166fc9bb53fc2aa5f3c
mah <you@example.com>
2023-03-15 14:06:09 +0330

    first commit

---
+++ lib/view/splash.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'login.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(Duration(seconds: 2), () {Get.to(Login()); });
+    return Scaffold(
+      body: Center(
+        child: Hero(tag: "logo", child: Image.asset("assets/images/twitter.png")),
+      ),
+    );
+  }
+}
